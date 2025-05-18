a3b0a7dccded36c672316c77782f1954072ee285
Pik4xe <ericsemprini99@gmail.com>
2025-05-19 00:50:43 +0200

    Feat: started translation, added fn const binpow to have the binary exponentiation, "a" is the base, "b" is the exponent and "mod_val" is the module used. teorically this is executed at compile time

--- src/pi.rs
+++ src/pi.rs
@@ -21,3 +21,18 @@ const fn power(base: f64, exp: i32) -> f64 {
         }
     }
 }
+
+pub const fn binpow(a: u64, mut b: u64, mod_val: u64) -> u64 {
+    let mut a = a % mod_val;
+    let mut result: u64 = 1;
+
+    while b > 0 {
+        if b & 1 == 1 {
+            result = (result * a) % mod_val;
+        }
+        a = (a * a) % mod_val;
+        b >>= 1;
+    }
+
+    result
+}
